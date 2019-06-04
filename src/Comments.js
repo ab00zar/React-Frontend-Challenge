48b0722c79f20cf01a9a24d53b56570103724b4a
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2019-06-04 16:15:05 +0200

    comments component is added to handle comments

---
+++ src/Comments.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Comments extends Component{
+    render(){
+        return(
+            <div>
+                <h3>Author</h3>
+                <p>Comment text will go here!</p>
+            </div>
+        )
+    }
+}
+
+export default Comments
