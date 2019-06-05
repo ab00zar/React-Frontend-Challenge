020b54f798b3d739ca08ad172916f3b1866f2445
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2019-06-05 12:57:02 +0200

    Updates

---
+++ precache-manifest.8bc1c9ee26725cd16eccfd55ad7fefbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0b276b1de2ddb082dbb975df3925e8",
+    "url": "/React-Frontend-Challenge/index.html"
+  },
+  {
+    "revision": "e5705f605dbbacfe0328",
+    "url": "/React-Frontend-Challenge/static/css/main.a6376c74.chunk.css"
+  },
+  {
+    "revision": "8d7e71fc5f551f7cf40d",
+    "url": "/React-Frontend-Challenge/static/js/2.b85fd010.chunk.js"
+  },
+  {
+    "revision": "e5705f605dbbacfe0328",
+    "url": "/React-Frontend-Challenge/static/js/main.c59eb1d9.chunk.js"
+  },
+  {
+    "revision": "f05a8c484518e487280c",
+    "url": "/React-Frontend-Challenge/static/js/runtime~main.1e7f8488.js"
+  }
+]);
